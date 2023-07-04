7cc682cd22a3e39b21f64ed639586dada2a7bcf3
bismarkboateng <bismarkb609@gmail.com>
2023-07-04 23:08:55 +0000

    database models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Rep(models.Model):
+    name = models.CharField(max_length=200)
+    RepId = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=700)
+    content = models.TextField()
+    # add an image field here after installing pillow
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.title[:10]}"
+    
+
+
+
+class LectureRoom(models.Model):
+    class_room = models.CharField(max_length=10)
+    class_size = models.IntegerField()
+    building_name = models.CharField(max_length=600)
+    is_available = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return f"{self.class_room} - {self.class_size}"
--- core/settings.py
+++ core/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-1=*u#6djo&+)55hwb=9%5e(1!5wz)b9afw(-zu8-&w8*+iq-=b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
