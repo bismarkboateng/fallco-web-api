931f296507562f935a787107cb73649a03023197
bismarkboateng <bismarkb609@gmail.com>
2023-07-05 23:37:44 +0000

    implement lecture rooms endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ArticleListCreateView
+from .views import ArticleListCreateView, LectureRoomListView
 
 
 urlpatterns = [
-    path("articles/", ArticleListCreateView.as_view(), name="list articles")
+    path("articles/", ArticleListCreateView.as_view(), name="list articles"),
+    path("lecture-rooms/", LectureRoomListView.as_view(), name="lecture rooms")
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,14 @@
 from rest_framework import generics
-from .serializers import ArticleSerializer
-from .models import Article
+from .serializers import ArticleSerializer, LectureRoomSerializer
+from .models import Article, LectureRoom
 
 
 
 class ArticleListCreateView(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+
+class LectureRoomListView(generics.ListAPIView):
+    queryset = LectureRoom.objects.all()
+    serializer_class = LectureRoomSerializer
